76779e3737b1a3729422e8a4d533d778ea2b72e6
Amanjot Singh <149322821+amanjot73@users.noreply.github.com>
2026-03-28 11:02:42 +0530

    Balanced means difference of height of left and right tree must be <=1

---
+++ Binary_Tree_Revision/isBalanced.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+    Node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node* create(Node* &root){
+    cout<<"Enter Data : "<<endl;
+    int data;
+    cin>>data;
+    root = new Node(data);
+    if(data == -1){
+        return NULL;
+    }
+    cout<<"Enter element to be inserted in the left of "<<data<<endl;
+    root->left = create(root->left);
+    cout<<"Enter element to be inserted in the right of "<<data<<endl;
+    root->right = create(root->right);
+    return root;
+}
+int height(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int l = height(root->left);
+    int r = height(root->right);
+    return max(l,r)+1;
+}
+bool balanced(Node* root){
+    if(root == NULL){
+        return true;
+    }
+    bool l = balanced(root->left);
+    bool r = balanced(root->right);
+    bool diff = abs(height(root->left) - height(root->right))<=1;
+    if(l && r && diff){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+int main(){
+    Node* root = NULL;
+    root = create(root);
+    cout<<(balanced(root)?"Yes":"No");
+}
